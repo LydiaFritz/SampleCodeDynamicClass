fa000f1b20babe1d8f4d07f4a4bcf469fda210ae
Lydia <Lydia.Fritz@gcu.edu>
2022-01-23 09:52:54 -0700

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,21 @@
+//Lydia K Fritz
+//CST-210-SP22
+
+/*
+* this project contains a dynamic class.
+* A dynamic class is a class that has one or more instance variables
+* that are dynamically allocated.  This means that the constructor uses the 
+* new operator to request dynamic data.  
+* 
+* A dynamic class must have a user-defined destructor.
+* A user-defined destructor is called every time an instance of the class is
+* destroyed.  
+* 
+* This class also contains a copy constructor.  
+* A copy constructor is required so that when a new instance of the class
+* is initialized with an existing instance of the class, dynamic data can
+* be copied to the new object.
+* 
+* The class, MyAuthors.cpp, contains comments explaining each of the above methods 
+* in detail.
+*/
